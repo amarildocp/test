871e8807a05e17ead0785331c1c951b6afa7ef95
Amarildocp <a.trezhnjeva@commprog.com>
2022-03-08 10:35:19 +0000

    added manifest

---
+++ amarildoo/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Punëkërkues',
+    'version': '1.0',
+    'category': 'Shërbimi Kombëtar i Punësimit',
+    'description': """
+                         Moduli i Regjistrit të Punëkërkuesit
+===========================================================================
+ """,
+    'author': 'Communication Progress',
+    'website': 'http://www.commprog.com',
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
